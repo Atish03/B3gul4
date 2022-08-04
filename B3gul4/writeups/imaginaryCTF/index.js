da4aa324e6c9ca773623c9aa8df1b9b6cf0f5a3b
Atish03 <atishpersonal003@gmail.com>
2022-08-04 16:48:00 +0530

    latest commit

--- B3gul4/writeups/imaginaryCTF/index.js
+++ B3gul4/writeups/imaginaryCTF/index.js
@@ -1,15 +1,20 @@
 $(".button").click(function() {
     const obj = $(this).parent();
+    const but = $(this);
     if (obj.attr("data-stat") == "closed") {
         obj.find("span").eq(0).css({"display": "flex"});
         obj.find("div").eq(1).css({"display": "none"});
         obj.attr("data-stat", "opened");
-        $(this).html("CLOSE");
+        obj.css({"padding": "30px"});
+        but.css({"height": "35px", "width": "120px", "background-color": "aliceblue", "right": "-45.5%"});
+        but.html("CLOSE");
     }else {
         console.log(obj.attr("class"));
         obj.find("span").eq(0).css({"display": "none"});
         obj.find("div").eq(1).css({"display": "flex"});
         obj.attr("data-stat", "closed");
-        $(this).html("VIEW");
+        but.css({"height": "80px", "width": "100%", "background-color": "transparent", "right": "0%"});
+        obj.css({"padding": "0px"});
+        $(this).html("");
     }
 })
